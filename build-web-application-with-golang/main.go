aabe31eda2986b72dda12b488fdc7c20250d6592
Jon Balon <development@jonbalon.com>
2022-07-01 18:56:50 -0400

    Hello, Go

---
+++ build-web-application-with-golang/main.go
@@ -0,0 +1,9 @@
+// Source file belongs to the `main` package.
+package main
+
+import "fmt"
+
+// You need an entry function called `main`.
+func main() { 
+	fmt.Printf("Hello, world or 你好，世界 or Καλημέρα κόσμε or こんにちは世界")
+}
