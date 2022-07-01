01a1e2636457b52ad3e3036061bf03af66cb4500
Jon Balon <development@jonbalon.com>
2022-07-01 19:03:39 -0400

    Define Variables

--- build-web-application-with-golang/main.go
+++ build-web-application-with-golang/main.go
@@ -5,5 +5,28 @@ import "fmt"
 
 // You need an entry function called `main`.
 func main() { 
-	fmt.Printf("Hello, world or 你好，世界 or Καλημέρα κόσμε or こんにちは世界")
+
+	// DEFINE a variable with name `variableName` and type `type`.
+	var variableName type
+
+	// Define multiple variables `vname1`, `vname2`, and `vname3` with the same
+	// type `type`.
+	var vname1, vname2, vname3 type
+
+	// Define a variable `variableName` of type `type` with initial value `value`.
+	var variableName type = value;
+
+	// Define multiple variables `vname1`, `vname2`, `vname3` all having the same
+	// type `type` with initial values of `v1`, `v2`, and `v3` respectively.
+	var vname1, vname2, vname3 type = v1, v2, v3
+
+	// Can define three variables without the type and initialize their values.
+	var vname1, vname2, vname3 = v1, v2, v3
+
+	// Can remove var keyword.
+	// This is called "short assignment".
+	vname1, vname2, vname3 := v1, v2, v3
+
+	// _ is a special variable. Any value given to it will be ignored.
+	_, b = 34, 35
 }
