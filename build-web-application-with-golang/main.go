5f4b782c0544c23337eae44211e7777265b7e324
Jon Balon <development@jonbalon.com>
2022-07-01 20:30:11 -0400

    Array

--- build-web-application-with-golang/main.go
+++ build-web-application-with-golang/main.go
@@ -1,11 +1,15 @@
 package main
 
-// Variables which begin with a capital letter will be exported
-var ExportedVariable int64 = 123
-
-// Private variable
-var privateVariable int64 = 456
-
 func main() {
+	// Array of type int with size 10.
+	var arr [10]int
 
+	// Define int array of size 3 with 3 initial values.
+	a := [3]int{1,2,3}
+
+	// Define int array of size 3 with 3 initial values.
+	b := [10]int{1,2,3}
+
+	// Array of arrays.
+	doubleArray := [2][4]int{[4]int{1,2,3,4}, [4]int{5,6,7,8}}
 }
