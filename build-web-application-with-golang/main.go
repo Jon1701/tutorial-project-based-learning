2b0c01647691f07ebed4d16263911507a84be61b
Jon Balon <development@jonbalon.com>
2022-07-01 20:23:58 -0400

    Exported Variables

--- build-web-application-with-golang/main.go
+++ build-web-application-with-golang/main.go
@@ -1,19 +1,11 @@
 package main
 
+// Variables which begin with a capital letter will be exported
+var ExportedVariable int64 = 123
+
+// Private variable
+var privateVariable int64 = 456
+
 func main() {
-	// Keyword `iota` makes an `enum` which begins with 0.
-	const (
-		x = iota // x = 0
-		y = iota // y = 1
-		z = iota // z = 2
-		w // If there is no expression after the constant's name, it uses the
-		  // previous expression, which is `iota`, so w = 3
-	)
 
-	const v = iota // Once `iota` meets the keyword `const`, it resets to 0
-								 // v = 0
-
-	const (
-		e,f,g = iota, iota, iota // Since all on same line, they all have value 0.
-	)
 }
