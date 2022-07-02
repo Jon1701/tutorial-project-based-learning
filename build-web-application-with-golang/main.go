c27b54e7e8fbd507f9cc28ba4b36d2b5edab9b86
Jon Balon <development@jonbalon.com>
2022-07-01 21:35:58 -0400

    Map

--- build-web-application-with-golang/main.go
+++ build-web-application-with-golang/main.go
@@ -1,32 +1,19 @@
 package main
 
-import "fmt"
-
 func main() {
-	// Slices are arrays of dyanmic length.
- 
-	// Slice of type int.
-	var slice1 []int
-	fmt.Printf("slice1: %s\n", slice1)
+	// Maps are similar to dictionaries in Python, or JS objects, but with the
+	// limitation that all keys must have the same type, and all values must have
+	// the same type.
 
-	// Define a slice and initialize its data.
-	slice2 := []byte{'h','e','l','l', 'o','w','o','r','l','d'}
+	// Declare a map where the keys are of type `string` and values of type `int`.
+	var numbers map[string]int
 
-	// Create slices from slices.
-	slice2a := slice2[3:7] // interval [x,y) = takes from indices 3,4,5,6
-	fmt.Printf("slice2a: %s\n", slice2a)
+	// Use `make` function to initialize it.
+	num2 := make(map[string]int)
 
-	fmt.Printf("length of slice2: %d\n", len(slice2))
-	fmt.Printf("maximum length of slice2: %d\n", cap(slice2))
-	
-	// Create a new slice.
-	slice3 := []byte{'g','o','o','d','b','y','e','w','o','r','l','d'}
+	num2["one"] = 1 // Assign values to a key.
+	num2["ten"] = 10
+	num2["three"] = 3
 
-	// `slice4` is combined from `slice2` and `slice3`.
-	slice4 := append(slice2, slice3...)
-	fmt.Printf("slice4: %s\n", slice4) // helloworldgoodbyeworld
-
-	// `slice5` contains parts of `slice2` and `slice3`.
-	slice5 := append(slice2[0:5], slice3[1:4]...)
-	fmt.Printf("slice5: %s\n", slice5) // helloood
+	// Order is ambiguous.
 }
