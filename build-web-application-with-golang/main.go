0ef81a874a34135296f5be844721a23f9874d091
Jon Balon <development@jonbalon.com>
2022-07-01 20:14:31 -0400

    Error Types

--- build-web-application-with-golang/main.go
+++ build-web-application-with-golang/main.go
@@ -1,35 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+)
 
 func main() { 
-	// Go uses the UTF-8 character set.
-
-	var frenchHello string
-
-	var emptyString string = ""
-
-	no, yes, maybe := "no", "yes", "maybe"
-
-	japaneseHello := "ohayou"
-
-	frenchHello := "bonjour"
-
-	// Strings are immutable.
-	// Cannot change them after creation.
-	var s string = "hello"
-	s[0] = "c" // compile error
-
-	// Convert strings to []byte, modify, then convert back to string.
-	c := []byte(s) 	// convert to []byte 
-	c[0] = 'c' 			// modify
-	s2 := string(c) // convert to string
-
-	fmt.Printf("%s\n", a) // Print string to console.
-
-	// Combine two strings.
-	a := "hello,"
-	b := " world"
-	v := a + b
-	fmt.Printf("%s\n", v)
+	// Go has one `error` type for dealing with error messages.
+	err := errors.New("this is an error message")
 }
