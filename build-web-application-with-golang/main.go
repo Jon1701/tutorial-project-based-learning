4a16fe777f02df536e8ad253a647f40a0fa4c9d5
Jon Balon <development@jonbalon.com>
2022-07-01 21:39:18 -0400

    `make` and `new`

--- build-web-application-with-golang/main.go
+++ build-web-application-with-golang/main.go
@@ -1,19 +1,11 @@
 package main
 
 func main() {
-	// Maps are similar to dictionaries in Python, or JS objects, but with the
-	// limitation that all keys must have the same type, and all values must have
-	// the same type.
-
-	// Declare a map where the keys are of type `string` and values of type `int`.
-	var numbers map[string]int
-
-	// Use `make` function to initialize it.
-	num2 := make(map[string]int)
-
-	num2["one"] = 1 // Assign values to a key.
-	num2["ten"] = 10
-	num2["three"] = 3
-
-	// Order is ambiguous.
+	// `make` does memory allocation for built-in models:
+	//   - `map`
+	//   - `slice`
+	//   - `channel`
+	//
+	//  `new` does memory allocation for types
+	//   - returns a pointer
 }
