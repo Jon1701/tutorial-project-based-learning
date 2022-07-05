b46348a86f340fd8ffaf964d0cca013caf84bbe8
Jon Balon <development@jonbalon.com>
2022-07-05 19:24:47 -0400

    Update Client to use WebSockets

--- realtime-chat-go-react/frontend/src/App.js
+++ realtime-chat-go-react/frontend/src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+import { connect, sendMsg } from "./api";
 
 function App() {
+  const handleSend = () => {
+    console.log("hello");
+    sendMsg("Hello");
+  };
+
+  useEffect(() => {
+    // Connect to socket.
+    connect();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button type="button" onClick={handleSend}>
+          Hit
+        </button>
       </header>
     </div>
   );
