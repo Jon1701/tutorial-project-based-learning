5be5cb98f368698da2143035cb813249717abdfa
Jon Balon <development@jonbalon.com>
2022-07-04 21:59:47 -0400

    Struct with Embedded Fields

--- build-web-application-with-golang/main.go
+++ build-web-application-with-golang/main.go
@@ -2,40 +2,27 @@ package main
 
 import "fmt"
 
-type Person struct {
+type Human struct {
 	name string
 	age int
+	weight int
 }
 
-func isOlder(p1 Person, p2 Person) (Person, int) {
-	if (p1.age > p2.age) {
-		return p1, p1.age - p2.age
-	}
-
-	return p2, p2.age - p1.age;
+type Student struct {
+	Human // embedded field
+	specialty string
 }
 
 func main() {
-	var p Person // p is of type Person.
+	mark := Student{Human{"Mark", 25, 120}, "Computer Science"}
 
-	p.name = "Astaxie"
-	p.age = 25
+	fmt.Printf("Name is: %s\n", mark.name);
+	fmt.Printf("Age is: %d\n", mark.age);
+	fmt.Printf("Weight is: %d\n", mark.weight);
+	fmt.Printf("Specialty is: %s\n", mark.specialty)
 
-	fmt.Printf("The person's name is: %s\n", p.name)
-	fmt.Printf("The person's age is: %d\n", p.age)
+	mark.specialty = "AI"
+	fmt.Println("Mark changed his specialty")
+	fmt.Printf("Specialty is: %s\n", mark.specialty)
 
-	var tom Person
-	tom.name = "Tom"
-	tom.age = 18
-	bob := Person{age: 25, name: "Bob"}
-	paul := Person{"Paul", 43}
-
-	tb_Older, tb_diff := isOlder(tom, bob);
-	tp_Older, tp_diff := isOlder(tom, paul)
-	bp_Older, bp_diff := isOlder(bob, paul)
-
-	fmt.Printf("Between %s and %s, %s is older by %d years\n", tom.name, bob.name, tb_Older.name, tb_diff);
-	fmt.Printf("Between %s and %s, %s is older by %d years\n", tom.name, paul.name, tp_Older.name, tp_diff);
-	fmt.Printf("Between %s and %s, %s is older by %d years\n", bob.name, paul.name, bp_Older.name, bp_diff);
-	
 }
