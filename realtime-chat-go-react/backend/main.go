29e534e53f6b951f0883470ef11ede49d6d1215e
Jon Balon <development@jonbalon.com>
2022-07-05 19:13:00 -0400

    Creating a WebSocket Endpoint

--- realtime-chat-go-react/backend/main.go
+++ realtime-chat-go-react/backend/main.go
@@ -2,18 +2,72 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
-func HandleSimpleServer(w http.ResponseWriter, r *http.Request) {
+// Define an Upgrader. This will require a Read and Write buffer size.
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+
+	// Checks the Origin of the connection. For now, just allow any connection.
+	CheckOrigin: func (r *http.Request) bool {
+		return true
+	},
+}
+
+// Define a reader which will listed for new messages sent to our websocket
+// endpoint.
+func reader(conn *websocket.Conn) {
+	for {
+		// Read message.
+		messageType, p, err := conn.ReadMessage()
+
+		// If there was a problem reading the message, log it.
+		if err != nil {
+			log.Println(err)
+			return;
+		}
+
+		// Print out message for clarity.
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return;
+		}
+	}
+}
+
+// Define WebSocket endpoint.
+func HandlerServeWebSocket(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Host)
+
+	// Upgrade endpoint to a WebSocket.
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// Listen indefinitely for new messages coming through on our WebSocket
+	// connection.
+	reader(ws)
+}
+
+func HandlerRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Simple Server")
 }
 
-func setupRoutes() {
-	http.HandleFunc("/", HandleSimpleServer)
+func SetUpRoutes() {
+	http.HandleFunc("/", HandlerRoot)
+	http.HandleFunc("/ws", HandlerServeWebSocket)
 }
 
 func main() {
-	setupRoutes()
+	fmt.Println("Chat App v0.01")
+	SetUpRoutes()
 	http.ListenAndServe(":8080", nil)
 }
