9a16466b7aa8028ca3c7a2eb114f9acbc9475743
Jon Balon <development@jonbalon.com>
2022-07-04 21:03:23 -0400

    Control Statements - `goto`

--- build-web-application-with-golang/main.go
+++ build-web-application-with-golang/main.go
@@ -3,17 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	// If-else statement.
-	if x > 10 {
-		fmt.Println("x is greater than 10")
-	} else {
-		fmt.Println("x is less than or equal to 10")
-	}
+	i := 0
 
-	// Initialize x, then check
-	if x:= computedValue(); x > 10 {
-		fmt.Println("x is greater than 10")
-	} else {
-		fmt.Println("x is less than 10")
-	}
+	Here:
+		fmt.Println(i)
+		i++
+		goto Here
 }
