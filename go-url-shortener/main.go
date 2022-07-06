d3fbd9ae219eea7320463b1bcb1e7260c429a7a1
Jon Balon <development@jonbalon.com>
2022-07-05 20:06:02 -0400

    Basic Web Server

--- go-url-shortener/main.go
+++ go-url-shortener/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func HelloHandlerFunc (c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hey Go URL Shortener!",
+	})
+}
 
 func main() {
-	fmt.Printf("Hello Go URL Shortener! 🚀\n")
+	// Create default engine instance.
+	r := gin.Default()
+	r.GET("/", HelloHandlerFunc)
+
+	err := r.Run(":9808")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to start the web server - Error %v", err))
+	}
 }
